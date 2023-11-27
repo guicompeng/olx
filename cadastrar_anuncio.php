5ece2b01ec79eca6605952032ef8538617da5148
guicompeng <guicompeng@gmail.com>
2023-11-27 20:18:06 -0300

    fix: atualizar anuncio

--- cadastrar_anuncio.php
+++ cadastrar_anuncio.php
@@ -1,8 +1,61 @@
+<?php
+include 'db_connect.php';
+
+// Consulta para obter categorias do banco de dados
+$categorias_query = "SELECT Nome FROM categoria";
+$categorias_result = $conn->query($categorias_query);
+
+// Consulta para obter marcas do banco de dados
+$marcas_query = "SELECT Nome FROM marca";
+$marcas_result = $conn->query($marcas_query);
+
+// Inicializa variáveis para os valores padrão
+$anuncio_codigo = "";
+$anuncio_categoria = "";
+$anuncio_marca = "";
+$anuncio_modelo = "";
+$anuncio_placa = "";
+$anuncio_estado = "";
+$anuncio_cidade = "";
+$anuncio_km = "";
+$anuncio_titulo = "";
+$anuncio_ano_fab = "";
+$anuncio_ano_modelo = "";
+$anuncio_preco = "";
+
+// Verifica se o parâmetro 'codigo' está presente na URL
+if (isset($_GET['codigo'])) {
+    // Se sim, obtém o código do anúncio
+    $anuncio_codigo = $_GET['codigo'];
+    
+    // Consulta para obter os dados do anúncio com base no código
+    $consulta_anuncio = "SELECT *, c.nome as categoria, mar.nome as marca FROM anuncio a join modelo m on a.modelo_codigo_fipe = m.codigo_fipe join categoria c on m.categoria_nome = c.nome join marca mar on m.marca_nome = mar.nome WHERE a.codigo = $anuncio_codigo";
+    $resultado_anuncio = $conn->query($consulta_anuncio);
+    // Se o anúncio existe, preenche as variáveis com os valores do anúncio
+    if ($resultado_anuncio->num_rows > 0) {
+        $anuncio = $resultado_anuncio->fetch_assoc();
+
+        $anuncio_categoria = $anuncio['categoria'];
+        $anuncio_marca = $anuncio['marca'];
+        $anuncio_modelo = $anuncio['modelo_codigo_fipe'];
+        $anuncio_placa = $anuncio['placa'];
+        $anuncio_estado = $anuncio['localizacao_estado'];
+        $anuncio_cidade = $anuncio['localizacao_cidade'];
+        $anuncio_km = $anuncio['km'];
+        $anuncio_titulo = $anuncio['titulo'];
+        $anuncio_ano_fab = $anuncio['ano_fab'];
+        $anuncio_ano_modelo = $anuncio['ano_modelo'];
+        $anuncio_preco = $anuncio['preco'];
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Cadastrar Anúncio</title>
+    <title><?php echo isset($_GET['codigo']) ? 'Editar Anúncio' : 'Cadastrar Anúncio'; ?></title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 
@@ -97,7 +150,6 @@
 
 
 <?php
-include 'db_connect.php';
 
 // Consulta para obter categorias do banco de dados
 $categorias_query = "SELECT Nome FROM categoria";
@@ -129,9 +181,9 @@ $marcas_result = $conn->query($marcas_query);
 </nav>
 
 <div class="container">
-    <h2 class="pt-3">Cadastrar Anúncio</h2>
+    <h2 class="pt-3"><?php echo isset($_GET['codigo']) ? 'Editar Anúncio' : 'Cadastrar Anúncio'; ?></h2>
 
-    <form action="processar_cadastro.php" method="post">
+    <form action="<?php echo isset($_GET['codigo']) ? 'processar_edicao.php' : 'processar_cadastro.php'; ?>" method="post">
 
         <div class="row">
             <!-- Categoria -->
@@ -141,7 +193,8 @@ $marcas_result = $conn->query($marcas_query);
                     <option value=''></option>
                     <?php
                     while ($categoria = $categorias_result->fetch_assoc()) {
-                        echo "<option value='{$categoria['Nome']}'>{$categoria['Nome']}</option>";
+                        $selected = ($categoria['Nome'] == $anuncio_categoria) ? 'selected' : '';
+                        echo "<option value='{$categoria['Nome']}' $selected>{$categoria['Nome']}</option>";
                     }
                     ?>
                 </select>
@@ -154,7 +207,8 @@ $marcas_result = $conn->query($marcas_query);
                     <option value=''></option>
                     <?php
                     while ($marca = $marcas_result->fetch_assoc()) {
-                        echo "<option value='{$marca['Nome']}'>{$marca['Nome']}</option>";
+                        $selected = ($marca['Nome'] == $anuncio_marca) ? 'selected' : '';
+                        echo "<option value='{$marca['Nome']}' $selected>{$marca['Nome']}</option>";
                     }
                     ?>
                 </select>
@@ -165,15 +219,19 @@ $marcas_result = $conn->query($marcas_query);
             <!-- Modelo -->
             <div class="form-group col-md-6">
                 <label for="modelo">Modelo:</label>
-                <select class="form-control" id="modelo" name="modelo" disabled>
+                <select class="form-control" id="modelo" name="modelo">
                     <option value="" selected></option>
+                    <?php
+                    // Certifique-se de que $anuncio_modelo é o valor correto do modelo do anúncio
+                    echo "<option value='{$anuncio_modelo}' selected>{$anuncio_modelo}</option>";
+                    ?>
                 </select>
             </div>
 
             <!-- Placa -->
             <div class="form-group col-md-6">
                 <label for="placa">Placa:</label>
-                <input type="text" class="form-control" id="placa" name="placa">
+                <input type="text" class="form-control" id="placa" name="placa" value="<?php echo $anuncio['placa']; ?>">
             </div>
         </div>
 
@@ -188,7 +246,8 @@ $marcas_result = $conn->query($marcas_query);
                     $estados_query = "SELECT DISTINCT Estado FROM localizacao";
                     $estados_result = $conn->query($estados_query);
                     while ($estado = $estados_result->fetch_assoc()) {
-                        echo "<option value='{$estado['Estado']}'>{$estado['Estado']}</option>";
+                        $selected = ($estado['Estado'] == $anuncio_estado) ? 'selected' : '';
+                        echo "<option value='{$estado['Estado']}' $selected>{$estado['Estado']}</option>";
                     }
                     ?>
                 </select>
@@ -197,8 +256,12 @@ $marcas_result = $conn->query($marcas_query);
             <!-- Localização - Cidade -->
             <div class="form-group col-md-6">
                 <label for="localizacao_cidade">Cidade:</label>
-                <select class="form-control" id="localizacao_cidade" name="localizacao_cidade" disabled>
+                <select class="form-control" id="localizacao_cidade" name="localizacao_cidade">
                     <option value="" selected>Selecione um estado primeiro</option>
+                    <?php
+                    // Certifique-se de que $anuncio_cidade é o valor correto da cidade do anúncio
+                    echo "<option value='{$anuncio_cidade}' selected>{$anuncio_cidade}</option>";
+                    ?>
                 </select>
             </div>
         </div>
@@ -207,13 +270,14 @@ $marcas_result = $conn->query($marcas_query);
             <!-- KM -->
             <div class="form-group col-md-6">
                 <label for="km">KM:</label>
-                <input type="text" class="form-control" id="km" name="km">
+                <input type="text" class="form-control" id="km" name="km" value="<?php echo $anuncio['km']; ?>">
+
             </div>
 
             <!-- Título -->
             <div class="form-group col-md-6">
                 <label for="titulo">Título:</label>
-                <input type="text" class="form-control" id="titulo" name="titulo">
+                <input type="text" class="form-control" id="titulo" name="titulo" value="<?php echo $anuncio['titulo']; ?>">
             </div>
         </div>
 
@@ -221,23 +285,26 @@ $marcas_result = $conn->query($marcas_query);
             <!-- Ano de Fabricação -->
             <div class="form-group col-md-6">
                 <label for="ano_fab">Ano de Fabricação:</label>
-                <input type="text" class="form-control" id="ano_fab" name="ano_fab">
+                <input type="text" class="form-control" id="ano_fab" name="ano_fab" value="<?php echo $anuncio['ano_fab']; ?>">
             </div>
 
             <!-- Ano do Modelo -->
             <div class="form-group col-md-6">
                 <label for="ano_modelo">Ano do Modelo:</label>
-                <input type="text" class="form-control" id="ano_modelo" name="ano_modelo">
+                <input type="text" class="form-control" id="ano_modelo" name="ano_modelo" value="<?php echo $anuncio['ano_modelo']; ?>">
             </div>
         </div>
 
         <!-- Preço -->
         <div class="form-group">
             <label for="preco">Preço:</label>
-            <input type="text" class="form-control" id="preco" name="preco">
+            <input type="text" class="form-control" id="preco" name="preco" value="<?php echo $anuncio['preco']; ?>">
         </div>
 
-        <button type="submit" class="btn btn-primary">Salvar</button>
+        <input type="hidden" name="codigo" value="<?php echo $anuncio_codigo; ?>">
+
+        <button type="submit" class="btn btn-primary"><?php echo isset($_GET['codigo']) ? 'Atualizar' : 'Salvar'; ?></button>
+
     </form>
 </div>
 
